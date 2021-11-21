02dbc76ab3f6768a457dbfabdd355415ff22769f
Vladyslav <frontendvlados@gmail.com>
2021-11-21 21:34:08 +0200

    changes at JS and Index.html files and testing input

---
+++ script.js
@@ -0,0 +1,50 @@
+let input = document.getElementById('inputText');
+let ul = document.getElementById('list');
+
+
+document.getElementById('applyBtn').addEventListener('click', function() {
+    if (input.value) {
+        let li = document.createElement('li');
+        let liValue = document.createTextNode(input.value);
+        li.appendChild(liValue);
+        ul.appendChild(li);
+        li.style.backgroundColor = 'yellow';
+        li.onclick = function(event) {
+            event.target = li.style.backgroundColor = 'green';
+        }
+        input.value = '';
+        input.style.backgroundColor = 'transparent';
+
+        let btn = document.createElement('button');
+        let btnName = document.createTextNode('Удалить');
+        btn.appendChild(btnName);
+        li.appendChild(btn);
+    } else {
+        input.style.backgroundColor = 'red';
+    }
+    
+    ul.onclick = removeItem;
+    function removeItem(event) {
+        var del = event.target;
+        if (del.tagName === "BUTTON") {
+          del.closest('li').remove();
+        }
+        return false;
+      }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
