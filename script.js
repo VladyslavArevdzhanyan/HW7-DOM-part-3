3c650da94d7445104af962dc505c2594cb51e9a2
Vladyslav <frontendvlados@gmail.com>
2021-11-23 23:22:54 +0200

    changes in JS file and write some new code

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 let input = document.getElementById('inputText');
 let ul = document.getElementById('list');
+let btn = document.getElementById('applyBtn');
 
-
-document.getElementById('applyBtn').addEventListener('click', function() {
+btn.addEventListener('click', function() {
     if (input.value) {
-        let li = document.createElement('li');
-        let liValue = document.createTextNode(input.value);
-        li.appendChild(liValue);
-        ul.appendChild(li);
-        li.onclick = function(event) {
-            event.target = li.classList.toggle('background');
+        function create() {
+            let li = document.createElement('li');
+            let liValue = document.createTextNode(input.value);
+            li.appendChild(liValue);
+            ul.appendChild(li);
+
+            let btnRemove = document.createElement('button');
+            let btnName = document.createTextNode('Удалить');
+            btnRemove.classList.add('btnRemove');
+            btnRemove.appendChild(btnName);
+            li.appendChild(btnRemove);
         }
+        create();
+
         input.value = '';
         input.style.backgroundColor = 'transparent';
-        let btn = document.createElement('button');
-        let btnName = document.createTextNode('Удалить');
-        btn.classList.add('btnRemove');
-        btn.appendChild(btnName);
-        li.appendChild(btn);
     } else {
         input.style.backgroundColor = 'red';
-    } 
+    };
 });
 
-document.getElementById('list').addEventListener('click', function removeItem(event) {
-    var del = event.target;
-    if (del.tagName === "BUTTON") {
-      del.closest('li').remove();
-    } return false;
-})
+ul.addEventListener('click', function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle('background')
+    } else if (event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+    }
+});
 
 
 
